5cbf2ced1cd3db60c1fe79e691e068ba3ba62527
stef-ang <anggara.stefanus12@mhs.if.its.ac.id>
2015-10-29 21:33:09 +0700

    first commit

---
+++ app/src/main/java/com/stef_developer/stalkingweather/activities/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.stef_developer.stalkingweather.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.stef_developer.stalkingweather.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
