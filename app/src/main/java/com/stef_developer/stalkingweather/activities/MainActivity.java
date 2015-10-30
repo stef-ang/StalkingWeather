a6759bc59472f1abce6846c336ac294fa994162a
Stefanus Anggara <anggara.stefanus@gmail.com>
2015-10-30 09:34:57 +0700

    Testing 1 done

--- app/src/main/java/com/stef_developer/stalkingweather/activities/MainActivity.java
+++ app/src/main/java/com/stef_developer/stalkingweather/activities/MainActivity.java
@@ -4,13 +4,20 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.github.kevinsawicki.http.HttpRequest;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.stef_developer.stalkingweather.R;
 import com.stef_developer.stalkingweather.adapter.WeatherAdapter;
 import com.stef_developer.stalkingweather.fragment.SettingDialog;
+import com.stef_developer.stalkingweather.models.DayResult;
 import com.stef_developer.stalkingweather.models.Result;
 
 import java.util.ArrayList;
@@ -20,8 +27,8 @@ public class MainActivity extends AppCompatActivity implements SettingDialog.ISe
 
     public static final String APPID = "61cbaa944c1df9998f3ce3fad6d623a1";
 
-    List<Result> listResult = new ArrayList<>();
-    RecyclerView.Adapter adapter;
+    List<DayResult> listResult = new ArrayList<>();
+    WeatherAdapter adapter;
     RecyclerView.LayoutManager layoutManager;
     RecyclerView recyclerView;
     ProgressBar progressBar;
@@ -35,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements SettingDialog.ISe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        layoutManager = new LinearLayoutManager(this);
         recyclerView = (RecyclerView) findViewById(R.id.rvWeather);
         recyclerView.setLayoutManager(layoutManager);
 
@@ -42,7 +50,7 @@ public class MainActivity extends AppCompatActivity implements SettingDialog.ISe
         fabSetting = (FloatingActionButton) findViewById(R.id.fabSetting);
         progressBar = (ProgressBar) findViewById(R.id.progress_bar);
 
-        adapter = new WeatherAdapter(listResult);
+        adapter = new WeatherAdapter(listResult, city.toUpperCase());
         recyclerView.setAdapter(adapter);
 
         loadContent(city, day);
@@ -57,7 +65,7 @@ public class MainActivity extends AppCompatActivity implements SettingDialog.ISe
         loadContent(_city, _day);
     }
 
-    private class LoadWeather extends AsyncTask<Void, Void, List<Result>> {
+    private class LoadWeather extends AsyncTask<Void, Void, Result> {
 
         String city;
         int days;
@@ -73,13 +81,36 @@ public class MainActivity extends AppCompatActivity implements SettingDialog.ISe
         }
 
         @Override
-        protected List<Result> doInBackground(Void... params) {
-            return null;
+        protected Result doInBackground(Void... params) {
+            String url = String.format("http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&mode=json&units=metric&cnt=%s&APPID=%s",
+                    this.city, String.valueOf(this.days), APPID);
+            Log.v("check_url", url);
+            HttpRequest request = HttpRequest.get(url);
+            String result = request.body();
+            Log.v("check_result", result);
+
+            return basic().fromJson(result, Result.class);
         }
 
         @Override
-        protected void onPostExecute(List<Result> objects) {
+        protected void onPostExecute(Result result) {
             progressBar.setVisibility(View.GONE);
+            if (result.getCod().equals("200")) {
+                adapter.setCity(result.getCity().getName());
+                listResult.addAll(result.getList());
+                adapter.notifyDataSetChanged();
+            } else {
+                Toast.makeText(MainActivity.this, "Pencarian cuaca pada kota " + city + " tidak ditemukan", Toast.LENGTH_LONG).show();
+            }
         }
+    }
+
+    public static Gson basic() {
+        final Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .serializeNulls()
+                .create();
+
+        return gson;
     }
 }
--- app/src/main/java/com/stef_developer/stalkingweather/adapter/WeatherAdapter.java
+++ app/src/main/java/com/stef_developer/stalkingweather/adapter/WeatherAdapter.java
@@ -1,10 +1,15 @@
 package com.stef_developer.stalkingweather.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.stef_developer.stalkingweather.models.Result;
+import com.stef_developer.stalkingweather.R;
+import com.stef_developer.stalkingweather.models.DayResult;
 
 import java.util.List;
 
@@ -15,20 +20,55 @@ public class WeatherAdapter extends RecyclerView.Adapter {
 
     private static final int TYPE_HEADER = 0;
     private static final int TYPE_ITEM = 1;
-    List<Result> items;
+    String city;
+    List<DayResult> items;
+    Context context;
 
-    public WeatherAdapter(List<Result> items) {
+    public WeatherAdapter(List<DayResult> items, String city) {
         this.items = items;
+        this.city = city;
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View v;
+        context = parent.getContext();
+        if (viewType == TYPE_HEADER) {
+            v = LayoutInflater.from(context).inflate(R.layout.item_header, parent, false);
+            return new WeatherAdapter.HeaderHolder(v);
+        } else if (viewType == TYPE_ITEM) {
+            v = LayoutInflater.from(context).inflate(R.layout.item, parent, false);
+            return new WeatherAdapter.ItemHolder(v);
+        }
+        throw new RuntimeException("there is no type that matches the type " + viewType + " + make sure your using types correctly");
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+//        ImageLoader imageLoader = ImageLoader.getInstance();
+        final DayResult item = items.get(position);
 
+        if (holder instanceof WeatherAdapter.HeaderHolder) {
+            HeaderHolder hh = (HeaderHolder) holder;
+            hh.tvTitleCity.setText(city);
+            // ih.tvToday.setText , timenya berupa millies
+            hh.tvTemp1.setText(item.getTemp().getDay() + "°");
+            hh.tvTemp2.setText(item.getTemp().getMin() + "°");
+            hh.tvTemp3.setText(item.getTemp().getMax() + "°");
+//            ih.img.setImageResource();
+            hh.tvStatus.setText(item.getWeather().get(0).getMain().toUpperCase());
+        } else if (holder instanceof WeatherAdapter.ItemHolder) {
+            ItemHolder ih = (ItemHolder) holder;
+            //    ih.img.setImageResource();
+//            ih.tvDate.setText();  , timenya berupa millies
+            ih.tvStatus.setText(item.getWeather().get(0).getMain().toUpperCase());
+            ih.tvTemp.setText(item.getTemp().getDay() + "°");
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position == 0 ? TYPE_HEADER : TYPE_ITEM;
     }
 
     @Override
@@ -37,16 +77,46 @@ public class WeatherAdapter extends RecyclerView.Adapter {
     }
 
     public static class ItemHolder extends RecyclerView.ViewHolder {
+        ImageView img;
+        TextView tvDate;
+        TextView tvStatus;
+        TextView tvTemp;
 
         public ItemHolder(View itemView) {
             super(itemView);
+
+            img = (ImageView) itemView.findViewById(R.id.img);
+            tvDate = (TextView) itemView.findViewById(R.id.tvDate);
+            tvStatus = (TextView) itemView.findViewById(R.id.tvStatus);
+            tvTemp = (TextView) itemView.findViewById(R.id.tvTemp);
         }
     }
 
     public static class HeaderHolder extends RecyclerView.ViewHolder {
+        TextView tvTitleCity;
+        TextView tvToday;
+        TextView tvTemp1, tvTemp2, tvTemp3;
+        ImageView img;
+        TextView tvStatus;
 
         public HeaderHolder(View itemView) {
             super(itemView);
+
+            tvTitleCity = (TextView) itemView.findViewById(R.id.tvTitleCity);
+            tvToday = (TextView) itemView.findViewById(R.id.tvToday);
+            tvTemp1 = (TextView) itemView.findViewById(R.id.tvTemp1);
+            tvTemp2 = (TextView) itemView.findViewById(R.id.tvTemp2);
+            tvTemp3 = (TextView) itemView.findViewById(R.id.tvTemp3);
+            img = (ImageView) itemView.findViewById(R.id.img);
+            tvStatus = (TextView) itemView.findViewById(R.id.tvStatus);
         }
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
     }
 }
